b9bf6090c487ba03b990a5ac40dcf76b61c243d4
loginovasvetlana <loginova.svetlana2018@yandex.ru>
2018-03-26 14:14:49 +0600

    Добавлен разрешённый host (имя пользователя на PythonAnywhere).

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hbd=@l*zdrbe$3c8o(ug&b!es)n1^tcpcok@*33u@gw^k7$9%&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'loginovasvetlana.pythonanywhere.com']
 
 
 # Application definition
